766ffd8fbf5e8fe7945156101e8ecb593acb0d22
Joe-Oliveri <57781435+Joe-Oliveri@users.noreply.github.com>
2022-08-03 20:37:15 -0400

    Update weatherStation4.ino

--- weatherStation4.ino
+++ weatherStation4.ino
@@ -11,8 +11,8 @@
 
 DHT dht(DHTPIN, DHTTYPE, 15);
 
-const char* ssid = "Vincent LAN Gogh";
-const char* password = "Acidburn1";
+const char* ssid = "xxxx";
+const char* password = "xxxx";
 
 unsigned long previousMillis = 0;
 unsigned long interval = 30000;
